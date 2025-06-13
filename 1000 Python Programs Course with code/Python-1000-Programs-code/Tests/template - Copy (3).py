7c021603beda4a3728bce99b8fc35c654c17721b
MusicalViking <arthur.belanger@maine.edu>
2025-06-13 18:32:48 -0400

    update 6-13-25

---
+++ 1000 Python Programs Course with code/Python-1000-Programs-code/Tests/template - Copy (3).py
@@ -0,0 +1,14 @@
+"""
+Author: Arthur Belanger
+Program: 
+Description:
+Notes:
+"""
+
+import time
+divider = ("---")
+print("Hello, Arthur! Your Python setup is working!")
+
+
+print("Arthur Belanger")
+print(time.ctime())
--- sanFoundryPrep/6.py
+++ sanFoundryPrep/6.py
@@ -3,14 +3,14 @@ Program Name
 Program description
 Author: Arthur Belanger
 Date created: 
-Notes:his is a Python Program to print all integers that aren�t divisible by either 2 or 3 and lies between 1 and 50.
+Notes:his is a Python Program to print all integers that aren't divisible by either 2 or 3 and lies between 1 and 50.
 
 Problem Description
-The program prints all integers that aren�t divisible by either 2 or 3 and lies between 1 and 50.
+The program prints all integers that aren't divisible by either 2 or 3 and lies between 1 and 50.
 
 Problem Solution
 1. Use a for-loop ranging from 0 to 51.
-2. Then use an if statement to check if the number isn�t divisible by both 2 and 3.
+2. Then use an if statement to check if the number isn't divisible by both 2 and 3.
 3. Print the numbers satisfying the condition.
 4. Exit.
 """
